1c42e2851d82b30344e1bae07baf3f7ab24e0031
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-06-13 22:01:04 -0300

    Concluido

--- plojetoJS02/script.js
+++ plojetoJS02/script.js
@@ -1,17 +1,28 @@
 function addTarefa() {
-     let mensagem = "Tarefa adicionada com sucesso!";
+     let mensagemSucesso = "Tarefa adicionada com sucesso!";
 
         let inputtarefa = document.getElementById("inputtarefa")
         // pega o valor do input 
-        let tarefa = inputtarefa.value
-        document.getElementById("mensagem").textContent = mensagem;
+        let tarefa = inputtarefa.value.trim()
+        
     // busca o elemento com o id
-    let listatr = document.getElementById("listatr")
-    let litr = document.createElement("li")
 
+    
+        inputtarefa.value = ""
+
+
+
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarrefa para adicioná-la a sua lista"
+        document.getElementById("mensagem").textContent = mensagemErro;
+    } else {
+        document.getElementById("mensagem").textContent = mensagemSucesso;
+        let listatr = document.getElementById("listatr")
+    let litr = document.createElement("li")
     litr.textContent = tarefa
 
     listatr.appendChild(litr)
 
-        inputtarefa.value = ""
+    }
+
     }
