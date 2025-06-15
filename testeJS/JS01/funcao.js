c06c5af8213a9b01388ee90b1509a12b27bf4fcd
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-06-15 15:24:59 -0300

    Concluido

---
+++ testeJS/JS01/funcao.js
@@ -0,0 +1,7 @@
+function adicionarlista(){
+    let paragrafo = "tarefa adicionada";
+    document.getElementById("paragrafo").textContent = paragrafo;
+
+    let inputtarefa = document.getElementById("inputtarefa")
+    let tarefa = inputtarefa.value
+}
