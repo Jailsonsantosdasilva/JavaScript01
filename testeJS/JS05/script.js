a8be1838d02cd823b05e9d7a32be30045c1e1fef
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-06-26 06:08:21 -0300

    c

---
+++ testeJS/JS05/script.js
@@ -0,0 +1,17 @@
+// console.log(5 == "5")
+// console.log(5 === "5")
+
+// let numero = 5;
+
+// if(numero == "5") {
+//     console.log(`E 5.`)
+// }
+
+let senhaCadastro = 1234"
+let senhaLogin =1234
+
+    if(senhaCadastro == senhaLogin) {
+        console.log(`Usuario logado`)
+    } else {
+        confirm.log(`Senha incorreta`)
+    }
