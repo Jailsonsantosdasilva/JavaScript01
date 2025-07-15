ccb5f5fdfd2e3c890e34ab42fc0c86820663c6bc
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-07-15 11:17:21 -0300

    feito

--- estudoJS/ex02/scrip.js
+++ estudoJS/ex02/scrip.js
@@ -1,20 +1,6 @@
-/*Arquivos JS aula 04 Arrays*/ 
-let produtos = ['arroz', 'feijao', 'leite']
-var codigos = Array(10, 20 ,30)
-let meses = ['jan', 'fev', 'mar', 'abr']
 
-//  ADICIONAR no final push = empurre
-produtos.push('Açucar')
-codigos.push(40, 50, 60, 70)
-meses.push('mai', 'jun', 'agr', '07')
-
-// REMOVE do final pop = estouro
-produtos.pop()
-codigos.pop()
-meses.pop()
-
-// ADICIONAR no inicio unshift
-produtos.unshift('Uva', 'maça')
-
-// REMOVER do inicio shift
-produtos.shift()
+let n1 = 5
+let n2 = 4
+let n3 = 5
+let soma = n1 + n3
+console.log(soma)
