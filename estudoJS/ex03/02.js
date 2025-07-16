2406ded445360f704284217becc820276e2b9a96
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-07-16 11:07:32 -0300

    feito

---
+++ estudoJS/ex03/02.js
@@ -0,0 +1,8 @@
+let temperatura = 30
+if(temperatura >= 30){
+    console.log('esta muito quente')
+} else if (temperatura >= 20){
+    console.log('esta agradavel')
+} else(
+    console.log('esta frio')
+)
