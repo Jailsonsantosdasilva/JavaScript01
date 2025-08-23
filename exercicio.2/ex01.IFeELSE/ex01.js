b1d6b4eced3bed96ef405066e08f8abcce05f559
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-08-23 15:47:52 -0300

    Concluido

---
+++ exercicio.2/ex01.IFeELSE/ex01.js
@@ -0,0 +1,13 @@
+const TamanhoCidade = 250000
+
+// if(nota >= 7){
+// console.log("Aprovado")
+// } else if (nota < 7) {
+//     console.log("Reprovado")
+// } 
+
+if (TamanhoCidade >= 3000.000){
+    console.log("Esta é uma cidade Grande")
+} else {
+    console.log("Esta é uma cidade Pequena")
+}
---
+++ exercicio.2/ex02.Functions/ex03.js
@@ -0,0 +1,41 @@
+function digaMeunome(){
+    console.log("JAILSON")
+}
+
+function QLQnome(nome){
+    console.log(nome)
+}
+
+QLQnome("Jailson")
+QLQnome("JJ")
+QLQnome("sla")
+
+// function soma(numero1, numero2){
+//     const Resultado = numero1 + numero1
+
+//     console.log(Resultado)
+// }
+
+// soma(10, 15)
+
+// Return
+
+function soma(numero1, numero2){
+    const Resultado = numero1 + numero2;
+
+    return Resultado;
+}
+
+const ResultadoSoma = soma(20, 10);
+console.log(ResultadoSoma);
+
+// Arraw Function
+const DigaNome = (nome, sobrenome, idade) => {
+    console.log("jailson", "silva", 19)
+}
+
+DigaNome()
+
+
+
+
