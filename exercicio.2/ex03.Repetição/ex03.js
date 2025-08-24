a6c0a576fbd3ad465ff13032ee8a7b8f4647c09f
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-08-24 00:05:42 -0300

    Concluido

---
+++ exercicio.2/ex03.Repetição/ex03.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+const user = {
+    name:"Rodolfo",
+    age:33,
+    street:"Rua dos Bobos"
+}
+
+for(let key in user){
+    console.log(key)
+}
---
+++ exercicio.2/ex05.forEach/ex05.js
@@ -0,0 +1,8 @@
+const users = [
+    {nome: "jailson,", age: 23,}
+]
+
+
+users.forEach(function(item,){
+    console.log(item);
+})
