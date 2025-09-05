d4772327e3143d399b6747b0c94f79cab3b88316
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-05 20:00:36 -0300

    Concluido

---
+++ Devclub/ex02/WebeDom.js
@@ -0,0 +1,8 @@
+const valorinput = document.querySelector("input")
+
+function converter(){
+    const valorconvertido = valorinput.value / 5
+
+    alert(`O valor convertido e ${valorconvertido}`)
+}
+
