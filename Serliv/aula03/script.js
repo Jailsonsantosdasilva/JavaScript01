fd9773f8a6a48751c85ee90efc8aa35f2cf7d5e9
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-25 15:28:41 -0300

    media js

---
+++ Serliv/aula03/script.js
@@ -0,0 +1,13 @@
+let numero1 = prompt("digite um numero")
+let numero2 = prompt("digite outro numero")
+
+numero1 = parseFloat(numero1)
+numero2 = parseFloat(numero2)
+
+let numero1N = Number(numero1)
+let numero2N = Number(numero2)
+
+soma = (numero1N + numero2N)/2;
+
+
+alert(`a media é: ${soma}`)
