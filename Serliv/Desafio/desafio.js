9a715aded76053ecf0cbffd0b9784f689da5c29d
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-25 00:07:05 -0300

    desafio

---
+++ Serliv/Desafio/desafio.js
@@ -0,0 +1,18 @@
+let nmr = prompt("digite seu numero");
+let nmrinteiro = parseInt(nmr);
+
+let nmr2 = prompt("digite outro numero");
+let nmr2inteiro = parseInt(nmr2)
+
+let somainteirosubtracao = nmrinteiro - nmr2inteiro;
+let somainteiromultiplicacao = nmrinteiro * nmr2inteiro;
+let somainteirodivisao = nmrinteiro / nmr2inteiro;
+let soma = nmrinteiro + nmr2inteiro;
+
+console.log(`a subtração e:${somainteirosubtracao}`)
+
+console.log(`a multiplicacao e:${somainteiromultiplicacao}`)
+
+console.log(`a divisão e:${somainteirodivisao}`)
+
+console.log(`a soma e:${soma}`)
---
+++ Serliv/aula02/js.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt("digite um numero"));
+let numero2 = Number(prompt("digite uma segunda vez"))
+
+let soma = numero1 + numero2;
+
+
+alert(`a soma dos numero são: ${soma}`) 
