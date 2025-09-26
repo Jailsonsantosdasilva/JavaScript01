8daf1e2b7e2900ab9662890ba351306f3a097186
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-26 11:13:20 -0300

    aula encerrada

---
+++ Serliv/aula08/script.js
@@ -0,0 +1,30 @@
+let numero = prompt("digite um numero");
+
+numeroI = Number(numero);
+
+
+
+switch (numeroI) {
+    case 1:
+        alert("que legal")
+        break;
+    case 2:
+        alert("bom")
+        break;
+    case 3:
+        alert("ruim")
+        break;
+    case 4:
+        alert("pessimo")
+        break;
+    case 5:
+        alert("serio")
+        break;
+    case 6:
+        alert("demais")
+        break;
+
+    default:
+        alert("tente novamente")
+        break;
+}
---
+++ Serliv/aula09/script.js
@@ -0,0 +1,15 @@
+let numero = prompt("digite um numero");
+numero1 = parseFloat(numero);
+
+let indice = 1;
+
+while (indice <= 10) {
+    alert(`${numero1} X ${indice} = ${numero * indice}`);
+
+    indice =  indice + 1;
+}
+
+
+
+
+
