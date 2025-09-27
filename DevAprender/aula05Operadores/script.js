c2b77eacf5f85a1f0ee1a765d8bd6de2be37b16a
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-26 21:51:02 -0300

    aula operadores

--- DevAprender/aula05Operadores/script.js
+++ DevAprender/aula05Operadores/script.js
@@ -23,4 +23,33 @@ console.log(idade);
 
 // Operador de Atribuição
 let valorTecladoGamer = 100;
+valorTecladoGamer += valorTecladoGamer;
 console.log(valorTecladoGamer);
+
+// Operadores de igualdadsde
+// igualdade estrita
+console.log(1 ===1)
+
+// operador ternário
+// tem um cliente, 100 premium, comum
+let pontos = 200;
+let tipo = pontos > 100 ? 'premium': 'comum';
+console.log(tipo);
+
+// Operadores logicos
+// operador (&&), retorna true se os dois operadores forem true
+console.log(true && true)
+
+
+let mairoDeIdade = false;
+let PossuiCarteirDeTrabalho = false;
+let podeAplicar = mairoDeIdade || PossuiCarteirDeTrabalho;
+console.log(podeAplicar)
+
+// operador logico ou (||)
+// Retonar true se um dos operadores forem true
+console.log('pode aplicar',podeAplicar)
+
+// operador NOT (!)
+let candidatoRecusado = !podeAplicar;
+console.log('recusado', candidatoRecusado)
