54ee628bca29e5727d12b316c9787f2780f7f98d
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-28 15:35:46 -0300

    aula10 save

--- DevAprender/aula09.For-Loop/script.js
+++ DevAprender/aula09.For-Loop/script.js
@@ -13,10 +13,43 @@
 //  }
 
 // while loop
-let i = 5;
+// let i = 5;
 
-while (i >= 1) {
-    if(i % 2 !== 0){
-        console.log(i)
-    }
+// while (i >= 1) {
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+// Do..while
+// let i = 0;
+
+// do {
+//     console.log("digitando", i)
+//     i++;
+// } while(i < 10);
+
+// for in 
+
+const pessoa = {
+    nome: "Jailson",
+    idade: 19,
+
+}
+
+// key-value
+for(let chave in pessoa){
+    console.log(chave,pessoa['nome'])
+}
+
+const cores = ['vermelho', 'azul', 'verde', 'preto', 'amarelo'];
+
+for(let indice in cores){
+    console.log(indice,cores[indice])
+}
+
+// for of
+
+for(let cor of cores){
+    console.log(cor)
 }
---
+++ DevAprender/aula10.MaximoDoisValores/script.js
@@ -0,0 +1,7 @@
+let ValorMaior = max(9,16)
+
+function max(numero1,numero2){
+ return numero1 > numero2 ? numero1: numero2;
+}
+
+console.log(ValorMaior)
