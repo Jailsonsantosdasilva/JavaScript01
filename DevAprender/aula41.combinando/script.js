490ca0b6ec1cd915619d82bdd6273b38da53030a
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-09-29 23:55:44 -0300

    save final

---
+++ DevAprender/aula41.combinando/script.js
@@ -0,0 +1,8 @@
+const numeros = [1,2,3,4,5];
+
+const combinando = numeros.join('.');
+console.log(combinando);
+
+const frase = "olha bem vindo ao curso";
+const resultado = frase.split('');
+console.log(resultado)
