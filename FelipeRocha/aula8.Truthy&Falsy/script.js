a2369adde673a69a0ded003aa289bba8626cfb61
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-04 22:30:20 -0300

    aula8 save

---
+++ FelipeRocha/aula8.Truthy&Falsy/script.js
@@ -0,0 +1,16 @@
+const x = "";
+// string vazia sera FALSE
+
+
+const y = 0;
+console.log(!!y);
+
+const a = null;
+const b = undefined;
+console.log(!!a);
+console.log(!!b);
+
+const list = [];
+const objeto = {};
+console.log(!![])
+console.log(!!objeto)
