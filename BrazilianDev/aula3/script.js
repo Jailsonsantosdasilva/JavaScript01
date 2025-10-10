2a93ffcec8fcc0b7e626f4985fbb39fbeaabe28a
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-10 15:05:39 -0300

    for of save

---
+++ BrazilianDev/aula3/script.js
@@ -0,0 +1,21 @@
+// For/Of Loop
+
+const nomes = ["joao", "lucas", "maria", "ana"];
+
+for (let nome of nomes){
+    console.log(`se chama: ${nome}`)
+}
+
+// exemplo da aula
+const ListaDeEstados = ["RJ", "SP", "MG"];
+
+for (let estados of ListaDeEstados){
+    console.log(`é um estado ${estados}`)
+}
+
+// outro exemplo
+
+const MyName = ["Jailson"];
+for (let meuNome of MyName){
+    console.log(`Meu nome e ${meuNome.toUpperCase()}`)
+}
---
+++ teste/ForOfLoop/script.js
@@ -0,0 +1,5 @@
+// For of 
+const numeros = [1, 2, 3, 4, 5];
+for(let Numeros of numeros){
+    console.log(Numeros)
+}
