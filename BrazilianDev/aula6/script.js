56f0ac98a21363bd292679607f05eb3df7bd4ea3
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-14 20:24:39 -0300

    henraca 2

--- BrazilianDev/aula6/script.js
+++ BrazilianDev/aula6/script.js
@@ -1,45 +1,44 @@
 class  Dev{
-    constructor(nome, idade,linguagem){
+    constructor(nome, idade,linguagem, tipoDeDev){
         this.nome = nome;
         this.idade = idade;
         this.linguagem = linguagem;
+        this.tipoDeDev = tipoDeDev;
     }
     saudacao(){
-        console.log(`oi Sou Dev Frontend, trabalho com ${this.linguagem} é me chamo ${this.nome} é tenho ${this.idade}\n\n`)
+        console.log(`oi Sou Dev ${this.tipoDeDev}, trabalho com ${this.linguagem} é me chamo ${this.nome} é tenho ${this.idade}\n\n`)
     }
 }
 
 // EXEMPLO
 
 class FrontEndDev extends Dev{
-    constructor(nome, idade,linguagem, framework){
+    constructor(nome, idade,linguagem, framework, tipoDeDev){
         super()
         this.nome = nome;
         this.idade = idade;
         this.linguagem = linguagem;
         this.framework = framework;
+        this.tipoDeDev = tipoDeDev;
     }
-    saudacao(){
-        console.log(`oi Sou Dev Frontend, trabalho com ${this.linguagem} é me chamo ${this.nome} é tenho ${this.idade}\n\n`)
-    }
+
 }
 
 class  BackendDev extends Dev{
-    constructor(nome, idade,linguagem, bancoDeDados){
+    constructor(nome, idade,linguagem, bancoDeDados, tipoDeDev){
         super()
         this.nome = nome;
         this.idade = idade;
         this.linguagem = linguagem;
         this.bancoDeDados = bancoDeDados;
+        this.tipoDeDev = tipoDeDev;
     }
-    saudacao(){
-        console.log(`oi Sou Dev Backend, trabalho com ${this.linguagem} é me chamo ${this.nome} é tenho ${this.idade}\n\n`)
-    }
+
 }
 
 
-const FrontEnd1 = new FrontEndDev("Jailson", 20, "JS", "React");
-const BackendDev1 = new BackendDev("Jailson", 20, "C#", "SQL server");
+const FrontEnd1 = new FrontEndDev("Jailson", 20, "JS", "React", "FrontEnd");
+const BackendDev1 = new BackendDev("Jailson", 20, "C#", "SQL server","BackEnd");
 
 
 console.log(FrontEnd1)
