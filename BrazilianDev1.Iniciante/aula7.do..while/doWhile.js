77062bad7c3649eb7dde0e91fbffa7e223e0be13
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-10-21 23:47:33 -0300

    doWhilesave

---
+++ BrazilianDev1.Iniciante/aula7.do..while/doWhile.js
@@ -0,0 +1,29 @@
+// Interadores clãssicos
+// DO WHILE
+// WHILE
+// FOR
+
+// ITERADORES DE ESTRUTURAS DE DADOS ITERÁVEIS
+// FOR IN
+// FOR OF
+
+// DO-WHILE LOOP
+
+// Exemplo
+
+let conte = 1
+do {
+    console.log("count is: " + conte)
+    conte++
+}while(conte < 6)
+
+// Exemplo 2
+let str = "hello";
+let count = 0;
+
+
+
+do {
+    console.log(str[count])
+    count++
+} while (count < str.length);
