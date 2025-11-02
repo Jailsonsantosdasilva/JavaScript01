1c36577253d535c00ca6689f7ebf6b46a248ba01
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-02 00:55:30 -0300

    teste 05 save

---
+++ teste/05/script.js
@@ -0,0 +1,14 @@
+let nome = prompt("qual seu nome");
+let num1 = prompt("digite um numero");
+let num2 = prompt("digite outro numero");
+
+nome.value;
+num1.vaalue;
+num2.value;
+
+num1N = Number(num1);
+num2N = Number(num2);
+
+let resultado = num1N + num2N;
+
+alert(`${nome} as soma dos numero informado são ${resultado}`)
