c2b4a7a0734ab39713b821722b27e4c208e90f54
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2025-11-24 21:00:58 -0300

    fixação javascript

---
+++ Matheus/While/while.js
@@ -0,0 +1,38 @@
+// Estrutura de repetição
+// Por exemplo: repetir uma determinada lógica em casa elementor de um array
+// as estruturas mais comuns são:while e for
+// Obs: tomar cuidado com o loop infinito
+
+// while
+
+// let x = 1;
+
+// while(x <= 5){
+//     console.log(`repetição ${x}`)
+//     x++;
+// }
+
+// let arr = ['teste', 'testando', 'w', 'e'];
+// let y = 0;
+
+// while(y <= 3){
+//     console.log(arr[y])
+//     y++
+// }
+
+
+// let myname = ['jailson', 'silva', 'santos']
+// let i = 0
+
+// while(i <= 2){
+//     console.log(myname[i])
+//     i++
+// }
+
+let nome = 'Jailson'
+let j = 0
+
+while(j <= 6){
+    console.log(nome[j])
+    j++
+}
