3bead423b48c0654e6bd9c821ed73ae77b9fb831
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2026-01-03 20:44:34 -0300

    execicio

---
+++ exercicio/ex02.objetos/objt.js
@@ -0,0 +1,10 @@
+// Obijeto
+
+const dadosAlfredo = {
+    idade: 27,
+    nome: "alfredo",
+    endereco: "Rua do não sei",
+    NumeroIdentificador: 2948357845
+}
+
+console.log(`Nome é ${dadosAlfredo.nome} a idade ${dadosAlfredo.idade} é o endereço e ${dadosAlfredo.endereco} numero de identificaçao ${dadosAlfredo.NumeroIdentificador}`)
