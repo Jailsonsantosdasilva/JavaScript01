477e1414648034b4e76d82d662707c91e54c332e
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2026-01-10 00:47:40 -0300

    nao entendi nada

--- JsAssincrono/jsAssincrono/assincrono.js
+++ JsAssincrono/jsAssincrono/assincrono.js
@@ -1,33 +1,5 @@
-const loginUser = (email, password, onSuccess, onError) =>{
+const axios = require("axios")
 
-    setTimeout(() => {
-        const error = false;
-        
-        if(error){
-           return onerror(new Error("error in login"))
-        }
-        
-        
-        console.log("user logged")
-        callback({ email })
-    }, 1500);
-}
-
-const loginUserPromise = (email, password) => {
-    return new Promise((resolve, reject)=>{
-        const error = false
-
-        if(error){
-            reject(new Error("error in login"))
-        }
-
-        console.log("user logged")
-        resolve({email})
-    })
-}
-
-loginUserPromise("Jailson@gmail.com", "123456").then((user)=>{
-    console.log({user})
-}).catch((error)=>{
-    console.log({error})
+axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+    console.log({response})
 })
