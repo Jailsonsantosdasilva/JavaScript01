dc895f2aad3d2af48ce2f6870ce75fc1077903df
Jailson santos da silva <Jailsonsantos6672@gmail.com>
2026-01-17 01:45:12 -0300

    try catch concluido

---
+++ teste/testeApi/TryCatch/trycatch2.js
@@ -0,0 +1,15 @@
+// ReferenceError
+// let x;
+
+// try {
+//     x = y + 1
+// } catch (error) {
+//   console.log("erro", erro.name)   
+// }
+
+// SyntaxError
+try {
+    
+} catch (error) {
+      console.log("erro", erro.name) 
+}
